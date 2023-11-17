90e81477079d4f0071e8647f4d124adffe9769a9
localthunder <128249364+localthunder@users.noreply.github.com>
2023-11-17 22:03:14 +0000

    dot env #2

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     username: process.env.DATABASE_USERNAME,
@@ -11,9 +13,9 @@ module.exports = {
     //...test db config
   },
   production: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME_PRODUCTION,
+    password: process.env.DATABASE_PASSWORD_PRODUCTION,
+    database: process.env.DATABASE_NAME_PRODUCTION,
+    host: process.env.DATABASE_HOST_PRODUCTION,
     dialect: 'postgres',  }
 };

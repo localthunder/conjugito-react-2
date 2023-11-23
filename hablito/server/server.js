55d44ec1c265d2ff0b5341f87dd69c3afcb86a5b
localthunder <128249364+localthunder@users.noreply.github.com>
2023-11-23 17:52:24 +0000

    server updates

--- hablito/server/server.js
+++ hablito/server/server.js
@@ -14,11 +14,11 @@ sequelize.sync()
   .catch(err => console.error('Unable to connect to the database:', err));
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // or '*' for any origin
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
